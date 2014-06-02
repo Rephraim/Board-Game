85fcc9944403b1751fed04099cd51ab9d804acd3
Rephraim <puppy.roses@gmail.com>
2014-06-02 00:39:01 -0700

    Update and rename ChoiceEvent.java to Option.java

---
+++ Option.java
@@ -0,0 +1,36 @@
+public class Option {
+  
+  private String descrip;
+  private int changeT;
+  private int changeM;
+  private int changeH;
+  private int changeA;
+  
+  public Option (String desc, int cT, int cM, int cH, int cA) {
+    descrip = details;
+    changeT = cT;
+    changeM = cM;
+    changeH = cH;
+    changeA = cA;
+  }
+  
+  public String getDescrip() {
+    return descrip;
+  }
+  
+  public int getChangeT() {
+    return changeT;
+  }
+  
+  public int getChangeM() {
+    return changeM;
+  }
+  
+  public int getChangeH() {
+    return changeH;
+  }
+  
+  public int getChangeA() {
+    return changeA;
+  }
+}
