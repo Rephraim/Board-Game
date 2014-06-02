457f755f2ebe6f6672f208c0ad4d34b25a151a86
Rephraim <puppy.roses@gmail.com>
2014-06-02 00:50:33 -0700

    Update Option.java

--- Option.java
+++ Option.java
@@ -1,13 +1,16 @@
 public class Option {
   
   private String descrip;
+  private String statchanges;
+  
   private int changeT;
   private int changeM;
   private int changeH;
   private int changeA;
   
-  public Option (String desc, int cT, int cM, int cH, int cA) {
+  public Option (String desc, String changes, int cT, int cM, int cH, int cA) {
     descrip = details;
+    statchanges = changes;
     changeT = cT;
     changeM = cM;
     changeH = cH;
@@ -16,6 +19,10 @@ public class Option {
   
   public String getDescrip() {
     return descrip;
+  }
+   
+   public String getStatChanges() {
+    return statchanges;
   }
   
   public int getChangeT() {
