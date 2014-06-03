fbeb89b490ed2be8e78a8159db3e303bd02f51f0
Rephraim <puppy.roses@gmail.com>
2014-06-03 06:08:30 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,8 +29,8 @@ public class Main {
 		boolean req=true;
 
 		events.add(null);
-		for (int i=1; i<36; i++){
-			while (input.hasNextLine())
+		while (input.hasNextLine()) {
+			for (int i=1; i<36; i++) { 
 			title = input.nextLine();
 			y = input.nextLine(); //for ref on txt file, not needed
 			
@@ -41,6 +41,7 @@ public class Main {
 			two = new Option(input.nextLine(), input.nextLine(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt() );
 			three = new Option(input.nextLine(), input.nextLine(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt() );
 			events.add( new Event(title, one, two, three, req) );
+			}
 		}
 	}
 }
