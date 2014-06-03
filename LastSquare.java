892f66004d614bb5327e69defaef77e1444540a2
swang319 <cyberblossom@gmail.com>
2014-06-03 03:55:41 -0700

    Update LastSquare.java

--- LastSquare.java
+++ LastSquare.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class LastSquare extends JFrame {
   	private Player p;
   
   	public LastSquare(Player player) {
-		p = player;
 		super("Finale");
+		p = player;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		buildWindow();
 		setVisible(true);
@@ -18,43 +19,48 @@ public class LastSquare extends JFrame {
 		JLabel description = new JLabel("You made it! You finished senior year, but did you graduate?");
 		JLabel aa = new JLabel("Tardies: " + p.getTardies());
 		aa.setFont(aa.getFont().deriveFont(Font.BOLD));
+		JLabel a = new JLabel("");
+		//theLabel.setText("" + num);
 		if (p.getTardies() < 10) {
-		  JLabel a = new JLabel("Wow, what kind of senior are you?");
+		  a.setText("Wow, what kind of senior are you?");
 		} else if (p.getTardies() < 20) {
-		  JLabel a = new JLabel("You have to attend one of those Tardy Intervention things… But I guess it could be worse.");
+		  a.setText("You have to attend one of those Tardy Intervention things… But I guess it could be worse.");
 		} else {
-		  JLabel a = new JLabel("Sorry dude, they’re not letting you walk at graduation because of your horrendous attendance record.");
+		  a.setText("Sorry dude, they’re not letting you walk at graduation because of your horrendous attendance record.");
 		}
 		JLabel ba = new JLabel("Money: " + p.getMoney());
 		ba.setFont(ba.getFont().deriveFont(Font.BOLD));
+		JLabel b = new JLabel("");
 		if (p.getMoney() < 10) {
-		  JLabel b = new JLabel("You have literally no money. Maybe you should drop college for a garbage-truck-driving job. I heard they pay pretty well.");
+		  b.setText("You have literally no money. Maybe you should drop college for a garbage-truck-driving job. I heard they pay pretty well.");
 		} else if (p.getMoney() < 30) {
-		  JLabel b = new JLabel("Goodbye carefree teenage years, hello college loans. Make sure to stock up on ramen.");
+		  b.setText("Goodbye carefree teenage years, hello college loans. Make sure to stock up on ramen.");
 		} else if (p.getMoney() < 50) {
-		  JLabel b = new JLabel("You’ll get through college okay, but grad school may be an issue...");
+		  b.setText("You’ll get through college okay, but grad school may be an issue...");
 		} else {
-		  JLabel b = new JLabel("Pfffft, loans?  You’re loaded!");	
+		  b.setText("Pfffft, loans?  You’re loaded!");	
 		}
 		JLabel ca = new JLabel("Happiness: " + p.getHappiness());
 		ca.setFont(ca.getFont().deriveFont(Font.BOLD));
+		JLabel c = new JLabel("");
 		if (p.getHappiness() < 10) {
-		  JLabel c = new JLabel("You cry yourself to sleep every night while scrolling through Tumblr.");
+		  c.setText("You cry yourself to sleep every night while scrolling through Tumblr.");
 		} else if (p.getMoney() < 30) {
-		  JLabel c = new JLabel("You wonder what you’re doing with your life, but console yourself with steamed meat buns.");
+		  c.setText("You wonder what you’re doing with your life, but console yourself with steamed meat buns.");
 		} else if (p.getMoney() < 50) {
-		  JLabel c = new JLabel("Life is swell, but the future scares the soles off your feet.");
+		  c.setText("Life is swell, but the future scares the soles off your feet.");
 		} else {
-		  JLabel c = new JLabel("A+ for having an awesome life. #yoloswag");	
+		  c.setText("A+ for having an awesome life. #yoloswag");	
 		}
 		JLabel da = new JLabel("Academics: " + p.getAcademics());
 		da.setFont(da.getFont().deriveFont(Font.BOLD));
+		JLabel d = new JLabel("");
 		if (p.getAcademics() < 25) {
-		  JLabel d = new JLabel("You don’t have enough units to graduate. I guess you have to go through the fire again….");
+		  d.setText("You don’t have enough units to graduate. I guess you have to go through the fire again….");
 		} else if (p.getAcademics() < 50) {
-		  JLabel d = new JLabel("You get rescinded! Ohlone should be accepting more students.");
+		  d.setText("You get rescinded! Ohlone should be accepting more students.");
 		} else {
-		  JLabel d = new JLabel("Good job, you graduated!");	
+		  d.setText("Good job, you graduated!");	
 		}
 		mainPanel.add(description);
 		mainPanel.add(aa);
