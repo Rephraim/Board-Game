54b494f028d057368426709c44f61adfedd02ab6
swang319 <cyberblossom@gmail.com>
2014-06-03 03:54:09 -0700

    Update BoardTile.java

--- BoardTile.java
+++ BoardTile.java
@@ -1,26 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+
 
 public class BoardTile extends JFrame {
 	private JButton option1;
 	private JButton option2;
 	private JButton option3;
-	private Option d1;
-	private Option d2;
-	private Option d3;
-	private int tilenumber;
+	private Option opt1;
+	private Option opt2;
+	private Option opt3;
+	private int number;
 	private Event event;
 	
 	private Player player;
 
   	public BoardTile(int tileNumber, ArrayList<Event> list, Player p) {
+		super ("Square" + tileNumber);
+		number = tileNumber;
 		event = list.get(tileNumber);
 		player = p;
-		d1 = event.getChoiceA();
-		d2 = event.getChoiceB();
-		d3 = event.getChoiceC();
-		super ("Square" + tilenumber);
+		opt1 = event.getChoiceA();
+		opt2 = event.getChoiceB();
+		opt3 = event.getChoiceC();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		buildWindow();
 		setVisible(true);
@@ -29,15 +33,15 @@ public class BoardTile extends JFrame {
 	private void buildWindow() {
 		JPanel mainPanel = new JPanel();
 		JLabel description = new JLabel(event.getDescrip());
-		JLabel a = new JLabel(d1.getDescrip);
-		JLabel b = new JLabel(d2.getDescrip);
-		JLabel c = new JLabel(d3.getDescrip);
+		JLabel a = new JLabel(opt1.getDescrip);
+		JLabel b = new JLabel(opt2.getDescrip);
+		JLabel c = new JLabel(opt3.getDescrip);
 		option1 = new JButton("A");
-		option1.addActionListener(new abuttonListener);
+		option1.addActionListener(new abuttonListener());
 		option2 = new JButton("B");
-		option1.addActionListener(new bbuttonListener);
+		option1.addActionListener(new bbuttonListener());
 		option3 = new JButton("C");
-		option1.addActionListener(new cbuttonListener);
+		option1.addActionListener(new cbuttonListener());
 		mainPanel.add(description);
 		mainPanel.add(a);
 		mainPanel.add(b);
@@ -46,34 +50,34 @@ public class BoardTile extends JFrame {
 		mainPanel.add(option2);
 		mainPanel.add(option3);
 
-		add(MainPanel);
+		add(mainPanel);
 	}
 
 	private class abuttonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			player.changeTardies(d1.getChangeT());
-			player.changeMoney(d1.getChangeM());
-			player.changeHappiness(d1.getChangeH());
-			player.changeAcademics(d1.getChangeA());
-			JOptionPane.showMessageDialog(null, d1.getStatChanges, "Option A", JOptionPane.PLAIN_MESSAGE);
+			player.changeTardies(opt1.getChangeT());
+			player.changeMoney(opt1.getChangeM());
+			player.changeHappiness(opt1.getChangeH());
+			player.changeAcademics(opt1.getChangeA());
+			JOptionPane.showMessageDialog(null, opt1.getStatChanges, "Option A", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 	private class bbuttonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			player.changeTardies(d2.getChangeT());
-			player.changeMoney(d2.getChangeM());
-			player.changeHappiness(d2.getChangeH());
-			player.changeAcademics(d2.getChangeA());
-			JOptionPane.showMessageDialog(null, d2.getStatChanges, "Option B", JOptionPane.PLAIN_MESSAGE);
+			player.changeTardies(opt2.getChangeT());
+			player.changeMoney(opt2.getChangeM());
+			player.changeHappiness(opt2.getChangeH());
+			player.changeAcademics(opt2.getChangeA());
+			JOptionPane.showMessageDialog(null, opt2.getStatChanges, "Option B", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 	private class cbuttonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			player.changeTardies(d3.getChangeT());
-			player.changeMoney(d3.getChangeM());
-			player.changeHappiness(d3.getChangeH());
-			player.changeAcademics(d3.getChangeA());
-			JOptionPane.showMessageDialog(null, d3.getStatChanges, "Option C", JOptionPane.PLAIN_MESSAGE);
+			player.changeTardies(opt3.getChangeT());
+			player.changeMoney(opt3.getChangeM());
+			player.changeHappiness(opt3.getChangeH());
+			player.changeAcademics(opt3.getChangeA());
+			JOptionPane.showMessageDialog(null, opt3.getStatChanges, "Option C", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 }
